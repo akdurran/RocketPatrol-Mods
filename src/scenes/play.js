237efab88df452698434cb33975263472c108911
Areeb Durrani <akdurran@ucsc.edu>
2023-04-23 23:12:17 -0700

    fix menus

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -61,7 +61,7 @@ class Play extends Phaser.Scene {
     scoreConfig.fixedWidth = 0;
     this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
       this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-      this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart or Left Arrow for Menu', scoreConfig).setOrigin(0.5);
+      this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
       this.gameOver = true;
     }, null, this);
 
