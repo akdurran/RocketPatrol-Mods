e999e4c5faa331da2d3fde9d9386619ddded1282
Areeb Durrani <akdurran@ucsc.edu>
2023-05-03 16:36:27 -0700

    switch up speeds in parallax scrolling

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -167,8 +167,8 @@ class Play extends Phaser.Scene {
     if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
       this.scene.start("menuScene");
     }
-    this.starfield.tilePositionX -= 4;
-    this.starfield2.tilePositionX -=2;
+    this.starfield.tilePositionX -= 1;
+    this.starfield2.tilePositionX += 5;
     if (!this.gameOver) {
       this.p1Rocket.update();
       this.ship01.update();               // update spaceships (x3)
